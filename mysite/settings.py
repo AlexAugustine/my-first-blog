b78c2d7c5358644051343259a923adf338b3c99f
lijaalex <lijaalexhere@gmail.com>
2019-03-15 20:51:54 +0530

    next change

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
 
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+# Password validation hello lija
+# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
