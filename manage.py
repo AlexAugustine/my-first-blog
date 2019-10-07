3cc25412b26b79c7ea68acb382fb0845f50a1954
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2019-10-07 22:52:35 +0530

    Update to manage.py file

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+#this is manage.py for handling all major tasks
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
